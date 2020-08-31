b900e8ee1dc0f8495f3bf09e31682749d9d8a5a0
happou31 <happo31@gmail.com>
2020-08-31 12:25:02 +0900

    fix: フォーマット掛けてエラー直した、あとpath.resolve使うようにした

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,20 @@ const webpack = require("webpack");
 const path = require("path");
 const isDev = process.env.NODE_ENV !== "production";
 
-const outputPath = path.join(__dirname, "dist");
+const outputPath = path.resolve(__dirname, "dist");
 
 var main = {
   mode: isDev ? "development" : "production",
   target: "electron-main",
   devtool: isDev ? "source-map" : false,
-  entry: path.join(__dirname, "src", "index"),
+  entry: path.resolve(__dirname, "src", "index"),
   output: {
     filename: "index.js",
-    path: outputPath
+    path: outputPath,
   },
   node: {
     __dirname: false,
-    __filename: false
+    __filename: false,
   },
   module: {
     rules: [
@@ -24,40 +24,42 @@ var main = {
         test: /.ts?$/,
         include: [path.resolve(__dirname, "src")],
         exclude: [path.resolve(__dirname, "node_modules")],
-        loader: "ts-loader"
-      }
-    ]
+        loader: "ts-loader",
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".ts"]
+    extensions: [".js", ".ts"],
   },
   plugins: [
     // Module not found: Error: Can't resolve './lib-cov/fluent-ffmpeg' が出るのを防ぐ
     new webpack.DefinePlugin({
-      "process.env.FLUENTFFMPEG_COV": false
+      "process.env.FLUENTFFMPEG_COV": false,
     }),
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, "src", "renderer", "index.html")
-      },
-      {
-        from: path.join(__dirname, "images/**/*")
-      }
-    ])
-  ]
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "renderer", "index.html"),
+        },
+        {
+          from: path.resolve(__dirname, "images/*"),
+        },
+      ],
+    }),
+  ],
 };
 
 var renderer = {
   mode: isDev ? "development" : "production",
   target: "electron-renderer",
-  entry: path.join(__dirname, "src", "renderer", "index"),
+  entry: path.resolve(__dirname, "src", "renderer", "index"),
   devtool: isDev ? "inline-source-map" : false,
   output: {
     filename: "index.js",
-    path: path.resolve(outputPath, "scripts")
+    path: path.resolve(outputPath, "scripts"),
   },
   resolve: {
-    extensions: [".json", ".js", ".jsx", ".css", ".ts", ".tsx"]
+    extensions: [".json", ".js", ".jsx", ".css", ".ts", ".tsx"],
   },
   module: {
     rules: [
@@ -66,23 +68,23 @@ var renderer = {
         use: ["ts-loader"],
         include: [
           path.resolve(__dirname, "src"),
-          path.resolve(__dirname, "node_modules")
-        ]
-      }
-    ]
+          path.resolve(__dirname, "node_modules"),
+        ],
+      },
+    ],
   },
   plugins: [
     // Module not found: Error: Can't resolve './lib-cov/fluent-ffmpeg' が出るのを防ぐ
     new webpack.DefinePlugin({
-      "process.env.FLUENTFFMPEG_COV": false
-    })
-  ]
+      "process.env.FLUENTFFMPEG_COV": false,
+    }),
+  ],
 };
 
 // var worker = {
 //   mode: isDev ? "development" : "production",
 //   target: "electron-renderer",
-//   entry: path.join(__dirname, "src", "renderer", "WebWorker", "index"),
+//   entry: path.resolve(__dirname, "src", "renderer", "WebWorker", "index"),
 //   devtool: isDev ? "inline-source-map" : false,
 //   output: {
 //     filename: "worker.js",
@@ -110,16 +112,16 @@ var renderer = {
 //     }),
 //     new CopyWebpackPlugin([
 //       // {
-//       //   from: path.join(__dirname, "node_modules", "jsgif", "GIFEncoder.js")
+//       //   from: path.resolve(__dirname, "node_modules", "jsgif", "GIFEncoder.js")
 //       // },
 //       // {
-//       //   from: path.join(__dirname, "node_modules", "jsgif", "LZWEncoder.js")
+//       //   from: path.resolve(__dirname, "node_modules", "jsgif", "LZWEncoder.js")
 //       // },
 //       // {
-//       //   from: path.join(__dirname, "node_modules", "jsgif", "NeuQuant.js")
+//       //   from: path.resolve(__dirname, "node_modules", "jsgif", "NeuQuant.js")
 //       // },
 //       // {
-//       //   from: path.join(__dirname, "node_modules", "jsgif", "b64.js")
+//       //   from: path.resolve(__dirname, "node_modules", "jsgif", "b64.js")
 //       // }
 //     ])
 //   ]
